af6e48d56d05fd021b9138529ebd4e398ee7ecf4
egluna-dev <egluna@live.ca>
2022-10-10 13:22:58 +1100

    Removed portrait photo folder

--- utilities/file-rename/main.py
+++ utilities/file-rename/main.py
@@ -1,24 +1,27 @@
 import os
+import pathlib
 from check_dimensions import get_aspect_ratio
 
 # Enter path and folder/category name and then run script to rename files
 # in a sequential order
-path = "C:\\Users\\eglun\\Desktop\\gippsland_copy"
+path = "C:\\Users\eglun\Documents\\2021\\professionalportfolio\eglmedia\\backend\\public\\images"
 folder = 'Gippsland'
 
 
 def rename_folder_files(file_path, folder_name):
     os.chdir(file_path)
-    # print(os.getcwd())
+    print(f"Absolute path: {pathlib.Path().absolute()}")
+    print(f"File path: {os.getcwd()}")
+    print(os.listdir())
 
     for count, file in enumerate(os.listdir()):
         file_name, file_ext = os.path.splitext(file)
         file_name = f"{folder_name}{str(count + 1)}"
 
-        orientation = get_aspect_ratio(file)
+        # orientation = get_aspect_ratio(file)
 
-        new_name = f'{file_name}{file_ext}_{orientation}'
-        os.rename(file, new_name)
+        # new_name = f'{file_name}{file_ext}_{orientation}'
+        # os.rename(file, new_name)
 
 
 rename_folder_files(file_path=path, folder_name=folder)
