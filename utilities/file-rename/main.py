607b6e019ef4dc65cc3332e33c8223a51c9b566e
egluna-dev <egluna@live.ca>
2022-10-10 18:39:49 +1100

    Re edited photos and improved python file renaming script

--- utilities/file-rename/main.py
+++ utilities/file-rename/main.py
@@ -1,27 +1,35 @@
+import enum
 import os
 import pathlib
 from check_dimensions import get_aspect_ratio
 
 # Enter path and folder/category name and then run script to rename files
 # in a sequential order
-path = "C:\\Users\eglun\Documents\\2021\\professionalportfolio\eglmedia\\backend\\public\\images"
+root_path = "C:\\Users\eglun\Documents\\2021\\professionalportfolio\eglmedia\\backend\\public\\images\\collections\\"
 folder = 'Gippsland'
 
 
 def rename_folder_files(file_path, folder_name):
     os.chdir(file_path)
-    print(f"Absolute path: {pathlib.Path().absolute()}")
-    print(f"File path: {os.getcwd()}")
-    print(os.listdir())
 
-    for count, file in enumerate(os.listdir()):
-        file_name, file_ext = os.path.splitext(file)
-        file_name = f"{folder_name}{str(count + 1)}"
+    for subdir, dirs, files in os.walk(root_path):
+        collection_name = subdir.split('\\')[-1]
+        if not files:
+            pass
+        else:
+            for count, file in enumerate(files):
+                file_name, file_ext = os.path.splitext(file)
+                file_path = os.path.join(subdir, file)
 
-        # orientation = get_aspect_ratio(file)
+                orientation = get_aspect_ratio(file_path)
+                file_name = f"{collection_name}_{orientation}_{str(count + 1)}"
 
-        # new_name = f'{file_name}{file_ext}_{orientation}'
-        # os.rename(file, new_name)
+                new_name = f"{file_name}{file_ext}"
+                os.rename(file, new_name)
 
 
-rename_folder_files(file_path=path, folder_name=folder)
+rename_folder_files(file_path=root_path, folder_name=folder)
+
+# for file in files:
+#     if not collection_name:
+#         print("Not proper dir")
