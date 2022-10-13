56de51daae2f19a15224ea0e6fae77a59d7cc3f9
egluna-dev <egluna@live.ca>
2022-10-13 22:13:33 +1100

    Converted photos to webp for performance optimization

--- utilities/file-rename/main.py
+++ utilities/file-rename/main.py
@@ -3,25 +3,39 @@ from check_dimensions import get_aspect_ratio
 
 # Enter path and folder/category name and then run script to rename files
 # in a sequential order
-root_path = None  # PORTFOLIO_IMAGES_PATH env var
+root_path = "C:\\Users\\eglun\\Desktop\\Uluru Road"  # PORTFOLIO_IMAGES_PATH
 
 
-def rename_folder_files(file_path):
+def file_rename(current_file, current_collection_name, current_subdir, current_count):
+    """
+    Carries out renaming of file
+    """
+    file_name, file_ext = os.path.splitext(current_file)
+    current_file_path = os.path.join(current_subdir, current_file)
+    orientation = get_aspect_ratio(current_file_path)
+    file_name = f"{current_collection_name}_{orientation}_{str(current_count + 1)}"
+
+    new_name = f"{file_name}{file_ext}"
+    # Uncomment the line below to execute file renaming routine
+    os.rename(os.path.join(current_subdir, current_file),
+              os.path.join(current_subdir, new_name))
+
+
+def main(file_path):
+    """
+    Loops through main image collection folder.
+    For each collection, another loops is done to rename every photo
+    and convert from jpg to webp format by calling file_rename and convert_to_webp
+    """
     for subdir, dirs, files in os.walk(file_path):
         collection_name = subdir.split('\\')[-1]
         if not files:
             pass
         else:
             for count, file in enumerate(files):
-                file_name, file_ext = os.path.splitext(file)
-                current_file_path = os.path.join(subdir, file)
-                orientation = get_aspect_ratio(current_file_path)
-                file_name = f"{collection_name}_{orientation}_{str(count + 1)}"
-
-                new_name = f"{file_name}{file_ext}"
-                # Uncomment the line below to execute file renaming routine
-                # os.rename(os.path.join(subdir, file),
-                #           os.path.join(subdir, new_name))
+                # Use to rename files with their current extension and photo orientation
+                file_rename(current_File=file, current_collection_name=collection_name,
+                            current_subdir=subdir, current_count=count)
 
 
-rename_folder_files(file_path=root_path)
+main(file_path=root_path)
